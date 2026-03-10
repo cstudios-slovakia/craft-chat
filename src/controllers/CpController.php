e67471e191456842fce67883d8c9b2297284d61f
Cstudios <39524543+cstudios-slovakia@users.noreply.github.com>
2026-03-10 07:30:33 +0100

    Add full-text search and date range filters to conversation list

--- src/controllers/CpController.php
+++ src/controllers/CpController.php
@@ -11,12 +11,38 @@ class CpController extends Controller
 {
     public function actionIndex()
     {
-        $conversations = Conversation::find()
-            ->orderBy(['dateUpdated' => SORT_DESC])
-            ->all();
+        $request = Craft::$app->getRequest();
+        $q = $request->getParam('q');
+        $dateFrom = $request->getParam('dateFrom');
+        $dateTo = $request->getParam('dateTo');
+
+        $query = Conversation::find();
+
+        if ($q) {
+            $query->joinWith('messages');
+            $query->andFilterWhere([
+                'or',
+                ['like', '{{%craft_chat_conversations}}.summary', $q],
+                ['like', '{{%craft_chat_messages}}.content', $q]
+            ]);
+            $query->groupBy(['{{%craft_chat_conversations}}.id']);
+        }
+
+        if ($dateFrom) {
+            $query->andFilterWhere(['>=', '{{%craft_chat_conversations}}.dateCreated', date('Y-m-d 00:00:00', strtotime($dateFrom))]);
+        }
+
+        if ($dateTo) {
+            $query->andFilterWhere(['<=', '{{%craft_chat_conversations}}.dateCreated', date('Y-m-d 23:59:59', strtotime($dateTo))]);
+        }
+
+        $conversations = $query->orderBy(['{{%craft_chat_conversations}}.dateUpdated' => SORT_DESC])->all();
 
         return $this->renderTemplate('craft-chat/cp/index', [
             'conversations' => $conversations,
+            'q' => $q,
+            'dateFrom' => $dateFrom,
+            'dateTo' => $dateTo,
         ]);
     }
 
